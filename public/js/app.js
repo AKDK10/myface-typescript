f75d7c658337da203b0911012c387c933d277e2e
Mina Lui <Mina.Lui@softwire.com>
2023-07-20 13:36:55 +0100

    creating mobile menu

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+
 });
+
+const navBarButton = document.getElementById("nav-bar-button");
+navBarButton.addEventListener("click", function() {
+  const navBarMenu = document.getElementById("nav-links-container");
+  const icon = document.getElementById("button-icon");
+  if((navBarMenu).classList.contains("open")) {
+    navBarMenu.style.display = "none";
+    navBarMenu.classList.replace("open", "close");
+    icon.classList.replace("fa-times","fa-bars");
+  } else {
+    navBarMenu.style.display = "flex";
+    navBarMenu.classList.replace("close","open");
+    icon.classList.replace("fa-bars","fa-times");
+  }
+  
+  
+})
